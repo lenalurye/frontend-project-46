2fc7ab94adcb96e2b2d1852a285601ffd8d9c2ed
Lena Lurye <frau.lurye@gmail.com>
2024-07-28 14:42:45 +0200

    Get diff implementation

--- index.js
+++ index.js
@@ -3,5 +3,44 @@ import fileParse from './format.js';
 const diff = (filepath1, filepath2) => {
   const config1 = fileParse(filepath1);
   const config2 = fileParse(filepath2);
+  return diffConfigs(config1, config2);
 };
+
+const diffConfigs = (config1, config2) => {
+  // Collect unique keys.
+  const uniqueKeys = {};
+  for (const key of Object.keys(config1)) {
+    uniqueKeys[key] = 1;
+  }
+  for (const key of Object.keys(config2)) {
+    uniqueKeys[key] = 1;
+  }
+
+  // Sort unique keys.
+  const uniqueKeysArray = Object.keys(uniqueKeys);
+  uniqueKeysArray.sort();
+
+  // Output the difference.
+  let output = '';
+  for (const key of uniqueKeysArray) {
+    const has1 = config1.hasOwnProperty(key);
+    const has2 = config2.hasOwnProperty(key);
+    const value1 = config1[key];
+    const value2 = config2[key];
+    if (has1 && has2) {
+      if (value1 === value2) {
+        output += `    ${key}: ${value1}\n`;
+      } else {
+        output += `  - ${key}: ${value1}\n`;
+        output += `  + ${key}: ${value2}\n`;
+      }
+    } else if (has1) {
+      output += `  - ${key}: ${value1}\n`;
+    } else {
+      output += `  + ${key}: ${value2}\n`;
+    }
+  }
+  return `{\n${output}}`;
+}
+
 export default diff;
