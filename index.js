5d477123afc002f0ee126ebff2373b7a94ffa232
Lena Lurye <frau.lurye@gmail.com>
2024-07-30 09:55:08 +0200

    Linter errors fixed

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 import fileParse from './format.js';
 
-const diff = (filepath1, filepath2) => {
-  const config1 = fileParse(filepath1);
-  const config2 = fileParse(filepath2);
-  return diffConfigs(config1, config2);
-};
-
 const diffConfigs = (config1, config2) => {
   // Collect unique keys.
   const uniqueKeys = {};
-  for (const key of Object.keys(config1)) {
-    uniqueKeys[key] = 1;
-  }
-  for (const key of Object.keys(config2)) {
-    uniqueKeys[key] = 1;
-  }
+  Object.keys(config1).forEach((key) => { uniqueKeys[key] = 1; });
+  Object.keys(config2).forEach((key) => { uniqueKeys[key] = 1; });
 
   // Sort unique keys.
   const uniqueKeysArray = Object.keys(uniqueKeys);
@@ -22,14 +12,14 @@ const diffConfigs = (config1, config2) => {
 
   // Output the difference.
   let output = '';
-  for (const key of uniqueKeysArray) {
-    const has1 = config1.hasOwnProperty(key);
-    const has2 = config2.hasOwnProperty(key);
+  uniqueKeysArray.forEach((key) => {
+    const has1 = Object.hasOwn(config1, key);
+    const has2 = Object.hasOwn(config2, key);
     const value1 = config1[key];
     const value2 = config2[key];
     if (has1 && has2 && value1 === value2) {
       output += `    ${key}: ${value1}\n`;
-      continue;
+      return;
     }
     if (has1) {
       output += `  - ${key}: ${value1}\n`;
@@ -37,8 +27,14 @@ const diffConfigs = (config1, config2) => {
     if (has2) {
       output += `  + ${key}: ${value2}\n`;
     }
-  }
+  });
   return `{\n${output}}`;
+};
+
+const diff = (filepath1, filepath2) => {
+  const config1 = fileParse(filepath1);
+  const config2 = fileParse(filepath2);
+  return diffConfigs(config1, config2);
 };
 
 export default diff;
