3ef642cc9966bef2db6780332997e28575e0786f
Lena Lurye <frau.lurye@gmail.com>
2024-08-01 21:53:45 +0200

    Yaml parsing support

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,6 +1,13 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
 import diff from '../index.js';
 
-test('diff', () => {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pathToFixture = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
+
+test('diff json', () => {
   const want = [
     '{',
     '  - follow: false',
@@ -12,5 +19,20 @@ test('diff', () => {
     '}',
   ];
 
-  expect(diff('__fixtures__/file1.json', '__fixtures__/file2.json')).toBe(want.join('\n'));
+  expect(diff(pathToFixture('file1.json'), pathToFixture('file2.json'))).toBe(want.join('\n'));
+});
+
+test('diff yaml', () => {
+  const want = [
+    '{',
+    '  - follow: false',
+    '    host: hexlet.io',
+    '  - proxy: 123.234.53.22',
+    '  - timeout: 50',
+    '  + timeout: 20',
+    '  + verbose: true',
+    '}',
+  ];
+
+  expect(diff(pathToFixture('file1.yaml'), pathToFixture('file2.yml'))).toBe(want.join('\n'));
 });
--- format.js
+++ format.js
@@ -1,4 +1,13 @@
 import { readFileSync } from 'node:fs';
+import path from 'path';
+import yaml from 'js-yaml';
 
-const fileParse = (filepath) => JSON.parse(readFileSync(filepath));
+const fileParse = (filepath) => {
+  const extension = path.extname(filepath.toLowerCase());
+  const data = readFileSync(filepath);
+  if (extension === '.yml' || extension === '.yaml') {
+    return yaml.load(data);
+  }
+  return JSON.parse(data);
+};
 export default fileParse;
