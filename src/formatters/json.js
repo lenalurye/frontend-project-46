29870b2496ddabb29a076e1627c824a4e714c44a
Lena Lurye <frau.lurye@gmail.com>
2024-08-26 23:03:53 +0200

    Json formatter refactoring

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,20 +1,15 @@
 const keyPath = (path, command) => path.map((pathElement) => `${pathElement}.`).join('') + command.key;
 
-const format = (diff, path = []) => {
-  const output = [];
-  diff.forEach((command) => {
-    switch (command.type) {
-      case 'added':
-      case 'removed':
-      case 'changed':
-        output.push({ ...command, key: keyPath(path, command) });
-        break;
-      case 'nested':
-        output.push(...format(command.children, [...path, command.key]));
-        break;
-      default:
-    }
-  });
-  return output;
-};
+const format = (diff, path = []) => diff.flatMap((command) => {
+  switch (command.type) {
+    case 'added':
+    case 'removed':
+    case 'changed':
+      return { ...command, key: keyPath(path, command) };
+    case 'nested':
+      return format(command.children, [...path, command.key]);
+    default:
+      return [];
+  }
+});
 export default (diff) => JSON.stringify(format(diff), null, 2);
