9840473e80dafa28395e61aa128075b4d0d3c7de
Lena Lurye <frau.lurye@gmail.com>
2024-08-26 23:11:06 +0200

    Diff refactoring

--- src/diff.js
+++ src/diff.js
@@ -2,36 +2,27 @@ import fileParse from './parsers.js';
 import format from './formatters/index.js';
 import { isObject, uniqueKeys } from './utils.js';
 
-const diffObjects = (obj1, obj2) => {
-  const output = [];
-  uniqueKeys(obj1, obj2).forEach((key) => {
-    const has1 = Object.hasOwn(obj1, key);
-    const has2 = Object.hasOwn(obj2, key);
-    const value1 = obj1[key];
-    const value2 = obj2[key];
-    if (has1 && has2) {
-      if (isObject(value1) && isObject(value2)) {
-        output.push({ type: 'nested', key, children: diffObjects(value1, value2) });
-        return;
-      }
-      if (value1 === value2) {
-        output.push({ type: 'no change', key, value: value1 });
-        return;
-      }
-      output.push({
-        type: 'changed',
-        key,
-        oldValue: value1,
-        newValue: value2,
-      });
-    } else if (has1) {
-      output.push({ type: 'removed', key, oldValue: value1 });
-    } else {
-      output.push({ type: 'added', key, newValue: value2 });
+const diffObjects = (obj1, obj2) => uniqueKeys(obj1, obj2).map((key) => {
+  const has1 = Object.hasOwn(obj1, key);
+  const has2 = Object.hasOwn(obj2, key);
+  const val1 = obj1[key];
+  const val2 = obj2[key];
+  if (has1 && has2) {
+    if (isObject(val1) && isObject(val2)) {
+      return { type: 'nested', key, children: diffObjects(val1, val2) };
     }
-  });
-  return output;
-};
+    if (val1 === val2) {
+      return { type: 'no change', key, value: val1 };
+    }
+    return {
+      type: 'changed', key, oldValue: val1, newValue: val2,
+    };
+  }
+  if (has1) {
+    return { type: 'removed', key, oldValue: val1 };
+  }
+  return { type: 'added', key, newValue: val2 };
+});
 
 const diffFiles = (filepath1, filepath2, formatName) => {
   const config1 = fileParse(filepath1);
